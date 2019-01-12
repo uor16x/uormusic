e86333d5b74b0092392997dfcf92e67894adc049
oleg.yurevich <oyurevich@virtido.com>
2019-01-12 22:54:44 +0200

    Visual stuff

--- public/app/controller.js
+++ public/app/controller.js
@@ -1,27 +1,48 @@
 function MainController($scope, AuthService) {
+    $scope.music = {
+        playing: false
+    };
+
     $scope.auth = false;
     $scope.loading = true;
+    $scope.authData = {
+        username: '',
+        password: ''
+    };
+
     AuthService.authGet()
         .then(response => {
             if (response.data) {
                 AuthService.set(response.data);
                 $scope.auth = true;
             }
-            $scope.loading = false;
         })
-        .catch(err => alert(err.data));
+        .catch(err => alert(err.data))
+        .finally(() => {
+            $scope.loading = false;
+        });
 
     $scope.authPost = function () {
+        if (!$scope.authData.username) {
+            return alert('Username missing');
+        }
+        if (!$scope.authData.password) {
+            return alert('Password missing');
+        }
         $scope.loading = true;
-        AuthService.authPost({username: 'aaa', password: '434'})
+        AuthService.authPost($scope.authData)
             .then(response => {
                 if (response.data) {
                     AuthService.set(response.data);
                     $scope.auth = true;
                 }
-                $scope.loading = false;
+
             })
-            .catch(err => alert(err.data));
+            .catch(err => alert(err.data))
+            .finally(() => {
+                $scope.authData.password = '';
+                $scope.loading = false;
+            });
     };
 
     $scope.logout = function () {
@@ -30,8 +51,10 @@ function MainController($scope, AuthService) {
             .then(response => {
                 AuthService.set(null);
                 $scope.auth = false;
-                $scope.loading = false;
             })
-            .catch(err => alert(err.data));
+            .catch(err => alert(err.data))
+            .finally(() => {
+                $scope.loading = false;
+            });
     }
 }
