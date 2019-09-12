45e9fee12cd5c4bbaef5e9307287291e01eee3e7
oleg.yurevich <u0r.home@gmail.com>
2019-09-12 18:11:14 +0300

    Quick fix

--- public/app/controller.js
+++ public/app/controller.js
@@ -552,12 +552,13 @@ function MainController($scope, $location, $anchorScroll, $sce, debounce, AuthSe
             if ('mediaSession' in navigator) {
                 navigator.mediaSession.metadata = new MediaMetadata({
                     title: $scope.music.currentSongTitle,
+                    artis: $scope.music.currentPlayingPlaylistName
                 });
                 navigator.mediaSession.setActionHandler('previoustrack', () => {
-                    $scope.next();
+                    $scope.prev();
                 });
                 navigator.mediaSession.setActionHandler('nexttrack', () => {
-                    $scope.prev();
+                    $scope.next();
                 });
             }
         }
