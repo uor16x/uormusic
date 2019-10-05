1c4e18c3193601bd9c7b1c55af54eee5401a18bf
oleg.yurevich <u0r.home@gmail.com>
2019-10-05 20:42:37 +0300

    Alexa setup refactor 2

--- alexa.js
+++ alexa.js
@@ -1,38 +1,42 @@
 const sdk = require('ask-sdk');
 
-const handlers = {
-    LaunchRequestHandler: {
+/**
+ * Handlers
+ */
+const LaunchRequestHandler = {
         canHandle: function(handlerInput) {
-            return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-        },
-        handle: function(handlerInput) {
-            const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-            return handlerInput.responseBuilder
-                .speak(speechText)
-                .reprompt(speechText)
-                .withSimpleCard('Hello World', speechText)
-                .getResponse();
-        }
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
-    HelloWorldIntentHandler: {
+    handle: function(handlerInput) {
+    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+}
+};
+const HelloWorldIntentHandler =  {
         canHandle: function(handlerInput) {
-            return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-                && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-            },
-        handle: function(handlerInput) {
-            const speechText = 'Hello World!';
-            return handlerInput.responseBuilder
-                .speak(speechText)
-                .withSimpleCard('Hello World', speechText)
-                .getResponse();
-        }
-    }
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle: function(handlerInput) {
+    const speechText = 'Hello World!';
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+}
 };
 
 function getSkill() {
     return sdk.SkillBuilders
         .custom()
-        .addRequestHandlers(...handlers)
+        .addRequestHandlers(
+            LaunchRequestHandler,
+            HelloWorldIntentHandler
+        )
         .create();
 }
 
