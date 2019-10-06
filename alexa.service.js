369c0420617e5a0fa082cfd4d62b0d9b1f6133f5
oleg.yurevich <u0r.home@gmail.com>
2019-10-07 01:23:49 +0300

    Overview handler fix 2

--- alexa.service.js
+++ alexa.service.js
@@ -2,10 +2,10 @@ let app;
 
 const methods = {
     overview: async userId => {
-        const playlists = await app.services.user.get({ _id: userId }, false, ['playlists']);
+        const user = await app.services.user.get({ _id: userId }, false, ['playlists']);
         return {
-            playlists: playlists.length,
-            songs: playlists.reduce((acc, item) => {
+            playlists: user.playlists.length,
+            songs: user.playlists.reduce((acc, item) => {
                 acc += item.songs.length;
                 return acc;
             }, 0)
