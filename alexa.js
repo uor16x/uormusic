bc63a145444037767a7653b7cfa3548ee9e73900
oleg.yurevich <u0r.home@gmail.com>
2019-10-11 12:30:49 +0300

    Typo fix

--- alexa.js
+++ alexa.js
@@ -1,7 +1,7 @@
 const sdk = require('ask-sdk');
 const s3Adapter = require('ask-sdk-s3-persistence-adapter');
 const defaultAttrs = {
-    playbackSettings: {
+    playbackSetting: {
         loop: false,
         shuffle: false,
     },
@@ -278,7 +278,7 @@ const LoopOnHandler = {
     },
     async handle(handlerInput) {
         const attrs = await handlerInput.attributesManager.getPersistentAttributes();
-        attrs.playbackSettings.loop = true;
+        attrs.playbackSetting.loop = true;
         return handlerInput.responseBuilder
             .speak('Loop turned on')
             .addAudioPlayerPlayDirective(
@@ -299,7 +299,7 @@ const LoopOffHandler = {
     },
     async handle(handlerInput) {
         const attrs = await handlerInput.attributesManager.getPersistentAttributes();
-        attrs.playbackSettings.loop = false;
+        attrs.playbackSetting.loop = false;
         const nextSong = alexaService.findNext(attrs);
         return responseBuilder
             .addAudioPlayerPlayDirective(
